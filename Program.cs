98a6ab5743511284df10ef49df74dc3469f18493
Hemavathi987 <136674928+Hemavathi987@users.noreply.github.com>
2024-06-29 11:36:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace programContinue
+{
+    class demofour
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("output will sdfghj");
+            for (int i = 1; i < num; i++)
+            {
+                if (i == 3 || i==6)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+    jhgf
